d0845f2fab9c860cf5121891a1620704e7d8f8aa
devahtesham <ahte02254@gmail.com>
2022-12-03 12:46:05 +0500

    assignment code

---
+++ index.py
@@ -0,0 +1,8 @@
+# ============================= QUESTION 1====================
+
+#  Write a Python program which accepts the radius of a circle from the user and compute the area
+
+r= float(input("Enter the radius of a circle :"))
+print(r)
+b=(3.142*(r**2))
+print(int(b))
