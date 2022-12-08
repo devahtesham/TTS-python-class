5fec65900b40681d2132db11cba8d5159fe0d8df
devahtesham <ahte02254@gmail.com>
2022-12-08 12:53:12 +0500

    list adding elements method code

--- index.py
+++ index.py
@@ -1,8 +1,30 @@
-# ============================= QUESTION 1====================
+myList = ["python",20,True,"TTS-class"]
+# print("myList",myList )
+# print("old list",myList)
+# print(len(myList))
 
-#  Write a Python program which accepts the radius of a circle from the user and compute the area
+# METHODS FOR ADDING ELEMENT IN LIST
+# 1) append 
+# myList.append("new-element")
+# # 2) insert
+# myList.insert(2,"second index value")
+# print("new list",myList)
+# 3) extend
+# myList.extend(["monday","tuesday","Wednesday"])
+# print("new list",myList)
 
-r= float(input("Enter the radius of a circle :"))
-print(r)
-b=(3.142*(r**2))
-print(int(b))
+# COPY LIST
+# myList2 = myList              # pass by reference
+# myList2 = myList.copy()              # pass by value
+# # print("myList2",myList2)    
+# # print("myList2",myList2)    
+# myList.append("python programming")
+# print("myList",myList)
+# print("myList2",myList2)
+
+# DELETE ELEMENT IN A LIST
+# del keyword
+# print(myList)
+# del myList[1]   # delete from particular position 
+# print(myList)
+
