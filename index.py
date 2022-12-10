d987b70c93f830f79f66c6074439e70c8f810713
devahtesham <ahte02254@gmail.com>
2022-12-10 13:13:46 +0500

    list and tuples

--- index.py
+++ index.py
@@ -3,28 +3,69 @@ myList = ["python",20,True,"TTS-class"]
 # print("old list",myList)
 # print(len(myList))
 
-# METHODS FOR ADDING ELEMENT IN LIST
-# 1) append 
-# myList.append("new-element")
-# # 2) insert
-# myList.insert(2,"second index value")
-# print("new list",myList)
-# 3) extend
+#  METHODS FOR ADDING ELEMENT IN LIST
+#  1) append 
+#  myList.append("new-element")
+#  # 2) insert
+#  myList.insert(2,"second index value")
+#  print("new list",myList)
+#  3) extend
 # myList.extend(["monday","tuesday","Wednesday"])
 # print("new list",myList)
 
-# COPY LIST
-# myList2 = myList              # pass by reference
-# myList2 = myList.copy()              # pass by value
-# # print("myList2",myList2)    
-# # print("myList2",myList2)    
+#  COPY LIST
+# myList2 = myList                       # pass by reference
+#  myList2 = myList.copy()              # pass by value
+# print("myList2",myList2)    
+# print("myList2",myList2)    
 # myList.append("python programming")
 # print("myList",myList)
 # print("myList2",myList2)
 
 # DELETE ELEMENT IN A LIST
-# del keyword
-# print(myList)
-# del myList[1]   # delete from particular position 
-# print(myList)
+# 1) pop() function
+# print("before",myList)
+# delete_element = myList.pop()
+# 2) remove() function
+# myList.remove("TTS-class")
+# 3) del keyword
+# del myList[2]
+# print("after",myList)
+
+# ================== sorting list =============
+myList2 = ["hamza","basit","asad","arfa","hassan","basit","basit"]
+# update element in a list
+# myList2[3] = 'ahtesham'
+# myList2.sort()        # by default ascending 
+# myList2.sort(reverse=True)
+# print(myList2)
+# ========= reverse() function ==================
+# myList2.reverse()
+# print(myList2)
+# ========= count() function ==================
+# print(myList2.count("basit"))
+# ========= index() function ==================
+# print(myList2.index("basit"))
+
+# ================ ACCESSING ELEMENTS FROM A LIST ================
+# ans = myList2[7]      
+# print(ans)
+
+# =================== TUPLES ==============
+
+# my_tuple = ("hamza","basit","asad","arfa","hassan","basit","basit")
+# print(my_tuple[3])
+# print(type(my_tuple))
+# print(type(myList2))
+# str = "python"
+# # string_ans = str.upper()
+# string_ans = str.title()
+# print(string_ans)
+print("python","programming",sep="\n")
+
+
+
+
+
+
 
